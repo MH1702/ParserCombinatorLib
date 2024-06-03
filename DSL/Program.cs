8dc6eb111c368149642b5314e85279a020ce6394
TheHelpfulHelper <60725502+TheHelpfulHelper@users.noreply.github.com>
2024-06-03 14:27:20 +0200

    DSL Parser (Function Decl)

--- DSL/Program.cs
+++ DSL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ParserCombinatorLib;
+using System.Diagnostics;
 using static ParserCombinatorLib.Parser;
 
 namespace DSL {
@@ -13,30 +14,48 @@ namespace DSL {
 			Public,
 		}
 
+		readonly struct FunctionArg {
+			public required string Type { get; init; }
+			public required string Name { get; init; }
+			public required bool HasDefaultValue { get; init; }
+			public string? DefaultValue { get; init; }
+		}
+
 		static DSLParser() {
 			function_arg_parser = Sequence([
-				new() { ParserDirect = Many(Letter(), "fn_arg_type").Map(x => string.Concat(x)), Optional = false, Key = "fn_arg_type" },
+				new() { ParserDirect = Many(Letter(), "fn_arg_type", 1).Map(x => string.Concat(x)), Optional = false, Key = "fn_arg_type" },
 				new() { ParserDirect = Whitespace(), Optional = false },
-				new() { ParserDirect = Many(Letter(), "fn_arg_name").Map(x => string.Concat(x)), Optional = false, Key = "fn_arg_name" },
+				new() { ParserDirect = Many(Letter(), "fn_arg_name", 1).Map(x => string.Concat(x)), Optional = false, Key = "fn_arg_name" },
 				new() { ParserDirect = Sequence([
 					new() { ParserDirect = Whitespace(), Optional = false },
 					new() { ParserDirect = Char('=', "fn_arg_default_marker"), Optional = false },
 					new() { ParserDirect = Whitespace(), Optional = false },
 					new() { ParserDirect = Many(Letter(), "fn_arg_default_value").Map(x => string.Concat(x)), Optional = false, Key = "fn_arg_default_value" },
-				], "fn_arg"), Optional = false },
+				], "fn_arg_default_value").Map(x => x["fn_arg_default_value"]), Optional = true, Key = "fn_arg_default_value" },
 				new() { ParserDirect = Whitespace(), Optional = false },
-			], "fn_arg");
+			], "fn_arg").Map(x => {
+				var has_default_value = x.ContainsKey("fn_arg_default_value");
+
+				return new FunctionArg() { Name = x["fn_arg_name"], DefaultValue = has_default_value ? x["fn_arg_default_value"] : null, HasDefaultValue = has_default_value, Type = x["fn_arg_type"] };
+			});
 
 			function_args_parser = Sequence([
 				new() { ParserFunc = () => function_arg_parser, Optional = false, Key = "fn_args_first" },
 				new() { ParserDirect = Many(Sequence([
+					new() { ParserDirect = Whitespace(), Optional = true },
 					new() { ParserDirect = Char(',', ""), Optional = false },
+					new() { ParserDirect = Whitespace(), Optional = true },
 					new() { ParserFunc = () => function_arg_parser, Optional = false, Key = "fn_args_rest" },
-				], ""), ""), Optional = true, Key = "fn_args" },
+				], "").Map(x => x["fn_args_rest"]), ""), Optional = true, Key = "fn_args_rest" },
 			], "fn_args").Map(x => {
-				Dumpify.DumpExtensions.Dump(x);
+				var list = new List<FunctionArg>() { x["fn_args_first"] };
 
-				return x;
+				if (x.ContainsKey("fn_args_rest")) {
+					var y = (List<object>)x["fn_args_rest"];
+					list.AddRange(y.Cast<FunctionArg>());
+				}
+
+				return list;
 			});
 
 			function_parser = Sequence([
@@ -60,7 +79,9 @@ namespace DSL {
 				new() { ParserDirect = Whitespace(), Optional = true },
 				new() { ParserDirect = Char('}', "fn_body_end"), Optional = false },
 			], "fn").Map(x => {
-				Dumpify.DumpExtensions.Dump(x);
+				if (!x.ContainsKey("fn_args")) {
+					x["fn_args"] = new List<FunctionArg>();
+				}
 
 				return x;
 			});
@@ -76,7 +97,9 @@ namespace DSL {
 
 		internal class Program {
 			static void Main(string[] args) {
-				DSLParser.Parse("pub fn main(int i = asd) {  }");
+				var result = DSLParser.Parse("pub fn main(int i) {  }");
+
+				Dumpify.DumpExtensions.Dump(result);
 			}
 		}
 	}
